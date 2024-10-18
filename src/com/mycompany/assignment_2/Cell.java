815654df805fd207dbbec9d8a92ad35c131a80dd
mohamedayoub110 <98830324+mohamedayoub110@users.noreply.github.com>
2024-10-18 16:43:58 +0300

    Update Cell.java

--- src/com/mycompany/assignment_2/Cell.java
+++ src/com/mycompany/assignment_2/Cell.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.assignment_2;
+package assignment2;
 
 import java.awt.Color;
-
-/**
- *
- * @author Mohammed Ayoub
- */
 
 import javax.swing.*;
 
@@ -18,7 +9,9 @@ class Cell extends JButton {
     private boolean isFired;
 
     public Cell(Board playerBoard, boolean hasShip) {
-    	this.hasShip = hasShip;
+        this.hasShip = hasShip;
+        this.isFired = false;
+        this.setBackground(Color.BLUE);
 
     }
 
@@ -37,24 +30,24 @@ class Cell extends JButton {
     public void fire() {
         if(!isFired)
         {
-        	isFired = true;
-        	if(hasShip)
-        		this.setBackground(Color.RED);
-        	else
-        		this.setBackground(Color.WHITE);
-        }		
+            isFired = true;
+            if(hasShip)
+                this.setBackground(Color.RED);
+            else
+                this.setBackground(Color.WHITE);
+        }
     }
 
     public void reset() {
-    	hasShip = false;
-    	isFired = false;
-    	this.setBackground(Color.BLUE);
+        hasShip = false;
+        isFired = false;
+        this.setBackground(Color.BLUE);
     }
 
     @Override
     public String toString() {
         if (hasShip)
-        	return "1";
+            return "1";
         return "0";
     }
 }
